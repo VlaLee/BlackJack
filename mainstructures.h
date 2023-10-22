93fd51d109caa40c8e81ec8fe01367c688ca71ea
Vladislav <vladivektin@mail.ru>
2023-10-22 18:19:25 +0300

    создал основные структуры данных в mainstructures.h

---
+++ mainstructures.h
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace cards_characteristics {
+	enum CardSuit {
+		DIAMONDS,
+		HEARTS,
+		CLUBS,
+		SPADES
+	};
+
+	enum CardRank {
+		RANK_2,
+		RANK_3,
+		RANK_4,
+		RANK_5,
+		RANK_6,
+		RANK_7,
+		RANK_8,
+		RANK_9,
+		RANK_10,
+		RANK_JACK,
+		RANK_QUEEN,
+		RANK_KING,
+		RANK_ACE
+	};
+}
+
+using namespace cards_characteristics;
+
+struct Card {
+	CardSuit suit;
+	CardRank rank;
+};
