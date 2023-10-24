043c1d071e5740b3db1812492df2732d852b0b5b
Tofiq <tekasimov@edu.hse.ru>
2023-10-24 18:26:39 +0300

    Исправлен баг с выводом карт

--- output.cpp
+++ output.cpp
@@ -14,19 +14,19 @@ void output(vector <Card> playerCards, vector <Card> dilerCards) {
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < playerCards.size(); j++) {
 			switch (playerCards.at(j).rank) {
-			case RANK_2:	 cout << cardsUp[0] [i]; break;
-			case RANK_3:	 cout << cardsUp[1] [i]; break;
-			case RANK_4:	 cout << cardsUp[2] [i]; break;
-			case RANK_5:	 cout << cardsUp[3] [i]; break;
-			case RANK_6:	 cout << cardsUp[4] [i]; break;
-			case RANK_7:	 cout << cardsUp[5] [i]; break;
+			case RANK_2:	 cout << cardsUp[12] [i]; break;
+			case RANK_3:	 cout << cardsUp[11] [i]; break;
+			case RANK_4:	 cout << cardsUp[10] [i]; break;
+			case RANK_5:	 cout << cardsUp[9] [i]; break;
+			case RANK_6:	 cout << cardsUp[8] [i]; break;
+			case RANK_7:	 cout << cardsUp[7] [i]; break;
 			case RANK_8:	 cout << cardsUp[6] [i]; break;
-			case RANK_9:	 cout << cardsUp[7] [i]; break;
-			case RANK_10:    cout << cardsUp[8] [i]; break;
-			case RANK_JACK:  cout << cardsUp[9] [i]; break;
-			case RANK_QUEEN: cout << cardsUp[10][i]; break;
-			case RANK_KING:	 cout << cardsUp[11][i]; break;
-			case RANK_ACE:   cout << cardsUp[12][i]; break;
+			case RANK_9:	 cout << cardsUp[5] [i]; break;
+			case RANK_10:    cout << cardsUp[4] [i]; break;
+			case RANK_JACK:  cout << cardsUp[3] [i]; break;
+			case RANK_QUEEN: cout << cardsUp[2][i]; break;
+			case RANK_KING:	 cout << cardsUp[1][i]; break;
+			case RANK_ACE:   cout << cardsUp[0][i]; break;
 			default:                                 break;
 			}
 		}
@@ -49,19 +49,19 @@ void output(vector <Card> playerCards, vector <Card> dilerCards) {
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < playerCards.size(); j++) {
 			switch (playerCards.at(j).rank) {
-			case RANK_2:	 cout << cardsDown[0] [i]; break;
-			case RANK_3:	 cout << cardsDown[1] [i]; break;
-			case RANK_4:	 cout << cardsDown[2] [i]; break;
-			case RANK_5:	 cout << cardsDown[3] [i]; break;
-			case RANK_6:	 cout << cardsDown[4] [i]; break;
-			case RANK_7:	 cout << cardsDown[5] [i]; break;
+			case RANK_2:	 cout << cardsDown[12] [i]; break;
+			case RANK_3:	 cout << cardsDown[11] [i]; break;
+			case RANK_4:	 cout << cardsDown[10] [i]; break;
+			case RANK_5:	 cout << cardsDown[9] [i]; break;
+			case RANK_6:	 cout << cardsDown[8] [i]; break;
+			case RANK_7:	 cout << cardsDown[7] [i]; break;
 			case RANK_8:	 cout << cardsDown[6] [i]; break;
-			case RANK_9:	 cout << cardsDown[7] [i]; break;
-			case RANK_10:    cout << cardsDown[8] [i]; break;
-			case RANK_JACK:  cout << cardsDown[9] [i]; break;
-			case RANK_QUEEN: cout << cardsDown[10][i]; break;
-			case RANK_KING:	 cout << cardsDown[11][i]; break;
-			case RANK_ACE:   cout << cardsDown[12][i]; break;
+			case RANK_9:	 cout << cardsDown[5] [i]; break;
+			case RANK_10:    cout << cardsDown[4] [i]; break;
+			case RANK_JACK:  cout << cardsDown[3] [i]; break;
+			case RANK_QUEEN: cout << cardsDown[2][i]; break;
+			case RANK_KING:	 cout << cardsDown[1][i]; break;
+			case RANK_ACE:   cout << cardsDown[0][i]; break;
 			default: 		                           break;
 			}
 		}
@@ -71,19 +71,19 @@ void output(vector <Card> playerCards, vector <Card> dilerCards) {
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < dilerCards.size(); j++) {
 			switch (dilerCards.at(j).rank) {
-			case RANK_2:	 cout << cardsUp[0] [i]; break;
-			case RANK_3:	 cout << cardsUp[1] [i]; break;
-			case RANK_4:	 cout << cardsUp[2] [i]; break;
-			case RANK_5:	 cout << cardsUp[3] [i]; break;
-			case RANK_6:	 cout << cardsUp[4] [i]; break;
-			case RANK_7:	 cout << cardsUp[5] [i]; break;
+			case RANK_2:	 cout << cardsUp[12] [i]; break;
+			case RANK_3:	 cout << cardsUp[11] [i]; break;
+			case RANK_4:	 cout << cardsUp[10] [i]; break;
+			case RANK_5:	 cout << cardsUp[9] [i]; break;
+			case RANK_6:	 cout << cardsUp[8] [i]; break;
+			case RANK_7:	 cout << cardsUp[7] [i]; break;
 			case RANK_8:	 cout << cardsUp[6] [i]; break;
-			case RANK_9:	 cout << cardsUp[7] [i]; break;
-			case RANK_10:    cout << cardsUp[8] [i]; break;
-			case RANK_JACK:  cout << cardsUp[9] [i]; break;
-			case RANK_QUEEN: cout << cardsUp[10][i]; break;
-			case RANK_KING:	 cout << cardsUp[11][i]; break;
-			case RANK_ACE:   cout << cardsUp[12][i]; break;
+			case RANK_9:	 cout << cardsUp[5] [i]; break;
+			case RANK_10:    cout << cardsUp[4] [i]; break;
+			case RANK_JACK:  cout << cardsUp[3] [i]; break;
+			case RANK_QUEEN: cout << cardsUp[2][i]; break;
+			case RANK_KING:	 cout << cardsUp[1][i]; break;
+			case RANK_ACE:   cout << cardsUp[0][i]; break;
 			default:                                 break;
 			}
 		}
@@ -106,19 +106,19 @@ void output(vector <Card> playerCards, vector <Card> dilerCards) {
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < dilerCards.size(); j++) {
 			switch (dilerCards.at(j).rank) {
-			case RANK_2:	 cout << cardsDown[0] [i]; break;
-			case RANK_3:	 cout << cardsDown[1] [i]; break;
-			case RANK_4:	 cout << cardsDown[2] [i]; break;
-			case RANK_5:	 cout << cardsDown[3] [i]; break;
-			case RANK_6:	 cout << cardsDown[4] [i]; break;
-			case RANK_7:	 cout << cardsDown[5] [i]; break;
+			case RANK_2:	 cout << cardsDown[12] [i]; break;
+			case RANK_3:	 cout << cardsDown[11] [i]; break;
+			case RANK_4:	 cout << cardsDown[10] [i]; break;
+			case RANK_5:	 cout << cardsDown[9] [i]; break;
+			case RANK_6:	 cout << cardsDown[8] [i]; break;
+			case RANK_7:	 cout << cardsDown[7] [i]; break;
 			case RANK_8:	 cout << cardsDown[6] [i]; break;
-			case RANK_9:	 cout << cardsDown[7] [i]; break;
-			case RANK_10:    cout << cardsDown[8] [i]; break;
-			case RANK_JACK:  cout << cardsDown[9] [i]; break;
-			case RANK_QUEEN: cout << cardsDown[10][i]; break;
-			case RANK_KING:	 cout << cardsDown[11][i]; break;
-			case RANK_ACE:   cout << cardsDown[12][i]; break;
+			case RANK_9:	 cout << cardsDown[5] [i]; break;
+			case RANK_10:    cout << cardsDown[4] [i]; break;
+			case RANK_JACK:  cout << cardsDown[3] [i]; break;
+			case RANK_QUEEN: cout << cardsDown[2][i]; break;
+			case RANK_KING:	 cout << cardsDown[1][i]; break;
+			case RANK_ACE:   cout << cardsDown[0][i]; break;
 			default: 		                           break;
 			}
 		}
